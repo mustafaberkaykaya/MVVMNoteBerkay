ae3367cc110d9625f78e8b0418ac91d0d2216696
Berkay  Kaya <mberkaykaya@outlook.com>
2022-02-05 21:33:39 +0300

    Initial Commit

---
+++ MVVMNoteBerkay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMNoteBerkay
+//
+//  Created by Mustafa Berkay Kaya on 5.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
